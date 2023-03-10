a4be209124c8e9ffc43bd093b25d243bd12c4328
Ophenyah <mono.soren@hotmail.com>
2023-03-10 20:45:02 +0100

    A04 Done

--- assignments/A04/projection.hpp
+++ assignments/A04/projection.hpp
@@ -1,4 +1,4 @@
-
+#include <glm/gtx/string_cast.hpp>
 
 void SetupProjectionMatrices(Assignment04 *A, float Ar) {
 /**************
@@ -11,18 +11,39 @@ void SetupProjectionMatrices(Assignment04 *A, float Ar) {
 	
 	// Isometric projection
 	S = glm::mat4(1);
-	A->Matrix(1, S); // sets the matrix corresponding to piece 1
+    S = glm::rotate(glm::mat4(1),glm::radians(-45.0f),glm::vec3(0,1,0));
+    S = glm::rotate(glm::mat4(1),glm::radians(35.26f),glm::vec3(1,0,0)) * S;
+	S = glm::mat4(0.1f,0,0,0, 0,-Ar/10,0,0, 0,0,-0.01f,0, 0,0,0.5f,1)*S;
+
+    A->Matrix(1, S); // sets the matrix corresponding to piece 1
+ //   std::cout << Ar << std::endl;
+//   std::cout << glm::to_string(glm::mat4(0.1f,0,0,0, 0,-Ar/10,0,0, 0,0,-0.01f,0, 0,0,0.5f,1)) << std::endl;
 
 	// Dimetric
 	S = glm::mat4(1);
-	A->Matrix(2, S); // sets the matrix corresponding to piece 2
+    S = glm::rotate(glm::mat4(1),glm::radians(-45.0f),glm::vec3(0,1,0));
+    S = glm::rotate(glm::mat4(1),glm::radians(20.0f),glm::vec3(1,0,0)) * S;
+    S = glm::mat4(0.1f,0,0,0, 0,-Ar/10,0,0, 0,0,-0.01f,0, 0,0,0.5f,1)*S;
 
-	// Trimetric
-	S = glm::mat4(1);
-	A->Matrix(3, S); // sets the matrix corresponding to piece 3
+    A->Matrix(2, S); // sets the matrix corresponding to piece 2
 
-	// Cabinet
-	S = glm::mat4(1);
-	A->Matrix(4, S); // sets the matrix corresponding to piece 4
+    // Trimetric
+    S = glm::mat4(1);
+    S = glm::rotate(glm::mat4(1),glm::radians(-25.0f),glm::vec3(0,1,0));
+    S = glm::rotate(glm::mat4(1),glm::radians(10.0f),glm::vec3(1,0,0)) * S;
+    S = glm::mat4(0.1f,0,0,0, 0,-Ar/10,0,0, 0,0,-0.01f,0, 0,0,0.5f,1)*S;
+
+    A->Matrix(3, S); // sets the matrix corresponding to piece 3
+
+
+
+   // Cabinet
+    S = glm::mat4(1);
+
+    S = glm::mat4(1,0,0,0,0,1,0,0,-0.5* 0.7,-0.5*0.7,1,0,0,0,0,1);
+    S = glm::mat4(0.1f,0,0,0, 0,-Ar/10,0,0, 0,0,-0.01f,0, 0,0,0.5f,1)*S;
+
+
+    A->Matrix(4, S); // sets the matrix corresponding to piece 4
 
 }
