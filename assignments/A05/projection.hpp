6b84e81ed7704cb3efbcc5017daceed89772faf4
Ophenyah <mono.soren@hotmail.com>
2023-03-20 13:46:07 +0100

    A05 & A06 done

--- assignments/A05/projection.hpp
+++ assignments/A05/projection.hpp
@@ -1,4 +1,5 @@
-
+#define GLM_FORCE_RADIANS
+#include <glm/gtx/string_cast.hpp>
 
 void SetupProjectionMatrices(Assignment05 *A, float Ar) {
 /**************
@@ -11,25 +12,27 @@ void SetupProjectionMatrices(Assignment05 *A, float Ar) {
 	
 	// Fov-y = 60 deg
 	S = glm::mat4(1);
-    S = glm::perspective(glm::radians(60.f),Ar,0.05f,100.0f);
+    S = glm::perspective(glm::radians(60.0f),Ar,0.1f,50.0f);
+    S[1,1] *= -1;
 
 	A->Matrix(1, S); // sets the matrix corresponding to piece 1
 
 	// Fov-y = 105 deg
 	S = glm::mat4(1);
-    S = glm::perspective(glm::radians(105.f),Ar,0.05f,100.0f);
-
+    S = glm::perspective(glm::radians(105.f),Ar,0.1f,50.0f);
+    S[1,1] *= -1;
     A->Matrix(2, S); // sets the matrix corresponding to piece 2
 
 	// Fov-y = 25 deg
 	S = glm::mat4(1);
-    S = glm::perspective(glm::radians(25.0f),Ar,0.05f,100.0f);
-
+    S = glm::perspective(glm::radians(25.0f),Ar,0.1f,50.0f);
+    S[1,1] *= -1;
     A->Matrix(3, S); // sets the matrix corresponding to piece 3
 
 	// Right View, Fov-x = 45 deg
     //I'm not sure at all what we are supposed to do
-	S = glm::perspective(glm::radians(45.0f),Ar,0.05f,100.0f);
+	S = glm::perspective(glm::radians(45.0f),Ar,0.1f,50.0f);
+    S[1,1] *= -1;
     S = glm::rotate(S,glm::radians(90.0f),glm::vec3(0,1,0));
 	A->Matrix(4, S); // sets the matrix corresponding to piece 4
 
